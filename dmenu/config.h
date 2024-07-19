7b8c39d9263e310d0e97303116386a5f9442f9ea
Theo <135859956+theodoer6@users.noreply.github.com>
2024-07-19 23:58:10 +0100

    Add files via upload

---
+++ dmenu/config.h
@@ -0,0 +1,39 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 200;     /* Amount of opacity. 0xff is opaque             */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Monospace:size=16"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	//Nord 
+//	[SchemeNorm] = { "#E5E9F0", "#3B4252" },
+//	[SchemeSel] = { "#E5E9F0", "#88C0D0" },
+//	[SchemeOut] = { "#000000", "#00ffff" },
+	
+	[SchemeNorm] = { "#f1be9b", "#020914" },
+	[SchemeSel] = { "#f1be9b", "#565879"},
+	[SchemeOut] = { "#f1be9b", "#DA6748" },
+};
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 10;
+static unsigned int columns    = 2;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 0;
